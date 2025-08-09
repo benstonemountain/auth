84d39b4e1b34adf91569b2f6ee69ad60795a9833
StoneMountain <kohegyibence@yahoo.com>
2025-08-09 16:16:54 +0200

    start project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo: "home", pathMatch: "full"},
+    {path:"home", loadComponent: () => import("./components/home/home").then(m => m.Home) },
+    {path:"sign-up", loadComponent: () => import("./components/sign-up/sign-up").then(m => m.SignUp) },
+    {path:"login", loadComponent: () => import("./components/login/login").then(m => m.Login) },
+    {path:"landing-page", loadComponent: () => import("./components/landing-page/landing-page").then(m => m.LandingPage) },
+];
